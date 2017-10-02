33f5a45b6ab2c0019c83b4beca2e5e70e078dd06
Kieran Rye <ryki16@nti.umea>
2017-10-02 12:35:46 +0200

    first commit

---
+++ bussresa.java
@@ -0,0 +1,31 @@
+import javax.swing.*;
+
+/* Vad? viasr om det är skottår eller inte
+	Vem av? Kieran
+	När? 2:e Oktober 2017
+*/
+
+public class bussresa {
+	public static void main (String[] arg) {
+		Integer question = JOptionPane.showInputDialog("
+			Hur gamal är du?");
+
+		int question = Integer.parseInt(s);
+		if (0 => 10) {
+			JOptionPane.showMessageDialog(null, "Din bussresa är gratis!");
+		 } 
+		else if (11 => 19) {
+			JOptionPane.showMessageDialog(null, "Din bussresa kostar 15kr!")
+		else if (20 => 25) {
+			JOptionPane.showMessageDialog(null, "Din bussresa kostar 20kr!")
+		else if (26 => 64) {
+			JOptionPane.showMessageDialog(null, "Din bussresa kostar 25kr!")
+		else
+			JOptionPane.showMessageDialog(null, "Din bussresa är gratis!")
+		}
+		}
+		}
+
+	}
+
+}
